b9b73ee9c012c4b77b70fa233423029a7640eff9
some-boots <someboots1111@gmail.com>
2021-09-04 17:04:00 -0500

    Add MAWP pressure corrector

--- pdf_plumber_attempt_per_cylinder.py
+++ pdf_plumber_attempt_per_cylinder.py
@@ -5,83 +5,83 @@ from tkinter.filedialog import askopenfilename, asksaveasfilename
 import os
 
 
-open_filepath = r'C:\Users\Jay\Desktop\Python\Auto Run File Builder\Tests\Test_15_F_9.PDF'
-save_filepath = r'C:\Users\Jay\Desktop\Python\Auto Run File Builder\Tests\Test_15_F_9.runM'
+# open_filepath = r'C:\Users\Jay\Desktop\Python\Auto Run File Builder\Tests\Test_15_F_9.PDF'
+# save_filepath = r'C:\Users\Jay\Desktop\Python\Auto Run File Builder\Tests\Test_15_F_9.runM'
 
-# open_filepath = None
-# save_filepath = None
-#
-# def open_file():
-#     global open_filepath
-#     open_filepath = askopenfilename(filetypes=[('PDF', '*.pdf')])
-#     open_var.set(open_filepath)
-#     if not open_filepath:
-#         return
-#
-# def save_file():
-#     global save_filepath
-#     save_filepath = asksaveasfilename(defaultextension='runM', filetypes=[("runM", "*.runM")])
-#     save_var.set(save_filepath)
-#     if not save_filepath:
-#         return
-#
-# # This function defines a message box that displays the appropriate error message
-# # when called with the error message and window title as arguments.
-# # We use this to alert the user if their input is invalid
-# def error_box(box_title, box_label):
-#     error_window = tk.Tk()
-#     error_window.title(box_title)
-#     error_window.geometry("500x100")
-#     error_lbl = tk.Label(master=error_window, text=box_label)
-#     error_lbl.pack()
-#     error_window.mainloop()
-#
-# # This function defines what happens when you click the Next button.  The variables
-# # are all designated as global to allow the program to use these values outside of the
-# # window.  Possible future upgrade would to use an object oriented approach here
-# # rather than the global variables.
-# def take_ent():
-#     # this is where we check that the source and save file paths have been provided.
-#
-#     if open_filepath == None or open_filepath == "":
-#         error_box("Enter All Fields", "Please select a source file.")
-#
-#     elif save_filepath == None or save_filepath == "":
-#         error_box("Enter All Fields", "Please select a save location.")
-#
-#     else:
-#         window.destroy()
-#
-# window = tk.Tk()
-# window.title("Auto Run File Builder")
-# window.geometry("900x600")
-#
-# #open source file location
-# frm_1 = tk.Frame(master = window)
-# frm_1.grid(row=0, column=0, padx=20, pady=(100,20), sticky='w')
-# btn_open = tk.Button(master = frm_1, text = "Select source file", width=20, command=open_file)
-# btn_open.grid(row=0, column=0)
-#
-# open_var = tk.StringVar()
-# lbl_open = tk.Label(master=frm_1, textvariable=open_var, width=70, relief=tk.SUNKEN, borderwidth=2)
-# lbl_open.grid(row=0, column=1, padx=20)
-#
-# #save result file location
-# frm_2 = tk.Frame(master=window)
-# frm_2.grid(row = 12, column = 0, padx = 20, pady = (50,50), sticky = 'w')
-# btn_save = tk.Button(master = frm_2, text='Results save location', width=20, command=save_file)
-# btn_save.grid(row=0, column=0)
-#
-# save_var = tk.StringVar()
-# lbl_save = tk.Label(master=frm_2, textvariable=save_var, width=70, relief=tk.SUNKEN, borderwidth=2)
-# lbl_save.grid(row=0, column=1, padx=20)
-#
-# # Next Button
-# btn = tk.Button(master=window, text="Next", width = 15, height = 3, command = take_ent)
-# btn.grid(row=15, column=1, sticky='se')
-#
-#
-# window.mainloop()
+open_filepath = None
+save_filepath = None
+
+def open_file():
+    global open_filepath
+    open_filepath = askopenfilename(filetypes=[('PDF', '*.pdf')])
+    open_var.set(open_filepath)
+    if not open_filepath:
+        return
+
+def save_file():
+    global save_filepath
+    save_filepath = asksaveasfilename(defaultextension='runM', filetypes=[("runM", "*.runM")])
+    save_var.set(save_filepath)
+    if not save_filepath:
+        return
+
+# This function defines a message box that displays the appropriate error message
+# when called with the error message and window title as arguments.
+# We use this to alert the user if their input is invalid
+def error_box(box_title, box_label):
+    error_window = tk.Tk()
+    error_window.title(box_title)
+    error_window.geometry("500x100")
+    error_lbl = tk.Label(master=error_window, text=box_label)
+    error_lbl.pack()
+    error_window.mainloop()
+
+# This function defines what happens when you click the Next button.  The variables
+# are all designated as global to allow the program to use these values outside of the
+# window.  Possible future upgrade would to use an object oriented approach here
+# rather than the global variables.
+def take_ent():
+    # this is where we check that the source and save file paths have been provided.
+
+    if open_filepath == None or open_filepath == "":
+        error_box("Enter All Fields", "Please select a source file.")
+
+    elif save_filepath == None or save_filepath == "":
+        error_box("Enter All Fields", "Please select a save location.")
+
+    else:
+        window.destroy()
+
+window = tk.Tk()
+window.title("Auto Run File Builder")
+window.geometry("900x600")
+
+#open source file location
+frm_1 = tk.Frame(master = window)
+frm_1.grid(row=0, column=0, padx=20, pady=(100,20), sticky='w')
+btn_open = tk.Button(master = frm_1, text = "Select source file", width=20, command=open_file)
+btn_open.grid(row=0, column=0)
+
+open_var = tk.StringVar()
+lbl_open = tk.Label(master=frm_1, textvariable=open_var, width=70, relief=tk.SUNKEN, borderwidth=2)
+lbl_open.grid(row=0, column=1, padx=20)
+
+#save result file location
+frm_2 = tk.Frame(master=window)
+frm_2.grid(row = 12, column = 0, padx = 20, pady = (50,50), sticky = 'w')
+btn_save = tk.Button(master = frm_2, text='Results save location', width=20, command=save_file)
+btn_save.grid(row=0, column=0)
+
+save_var = tk.StringVar()
+lbl_save = tk.Label(master=frm_2, textvariable=save_var, width=70, relief=tk.SUNKEN, borderwidth=2)
+lbl_save.grid(row=0, column=1, padx=20)
+
+# Next Button
+btn = tk.Button(master=window, text="Next", width = 15, height = 3, command = take_ent)
+btn.grid(row=15, column=1, sticky='se')
+
+
+window.mainloop()
 
 # # End of GUI portion
 # ######################################################################
@@ -504,6 +504,20 @@ def pressure_corrector(col_start, tot_cyl):
         ps_pd = [round(p * 14.6959, 3) for p in ps_pd]
     return ps_pd
 
+def mawp_corrector(press):
+    if pressure_for_output == "psi":
+        pass
+    elif pressure_for_output == "bar":
+        return int(5 * round((float(press) * 14.5038)/5))
+    elif pressure_for_output == "kPa":
+        return int(5 * round((float(press) * 0.145038)/5))
+    elif pressure_for_output == "MPa":
+        return int(5 * round((float(press) * 145.038)/5))
+    elif pressure_for_output == "kg/cm2":
+        return int(5 * round((float(press) * 14.2233)/5))
+    elif pressure_for_output == "atm":
+        return int(5 * round((float(press) * 14.6959)/5))
+
 # The stage_assigner takes the service as an argument and populates an output
 #  string with each cylinder of that stage in the runM format.
 
@@ -516,7 +530,7 @@ def cyl_assigner(total_cyls, stg, serv):
                         <Cylinder>
                             <throw>{stages[f'Service {serv} Stage {int(stg + 1)} Throws'][cyl]}</throw>
                             <model>{stages[f'Service {serv} Stage {stg + 1} Cylinder'][cyl][0]}</model>
-                            <mawp>{stages[f'Service {serv} Stage {stg + 1} Cylinder'][cyl][3]}</mawp>
+                            <mawp>{mawp_corrector(stages[f'Service {serv} Stage {stg + 1} Cylinder'][cyl][3])}</mawp>
                             <bore_size>{stages[f'Service {serv} Stage {stg + 1} Cylinder'][cyl][1]}</bore_size>
                             <action>{stages[f'Service {serv} Stage {stg + 1} Cylinder'][cyl][6]}</action>
                             <CESpacers>{stages[f'Service {serv} Stage {stg + 1} Cylinder'][cyl][8]}</CESpacers>
@@ -678,19 +692,6 @@ output_txt = fr"""<?xml version="1.0" encoding=UTF-8 ?>
 
 print(output_txt)
 
-# Current program limitaions include inability to select cylinders or cylinder
-#  action / clearance.  This means that the program will autoselect the cylinders
-#  attempting to match whatever volumen is given to target flow.  Currently,
-#  I have the target flow value set to the calc flow from the input .pdf so
-#  the program at least attempts to size the cylinders to match the actual
-#  throughput on the .pdf.  This if flawed b/c if the input .pdf has the first
-#  stg single acted for example, the program will select smaller cylinders
-#  goal seeking the volume from that configuration.
-#  Another limitation is that we can't set the ambient, or the cooler discharge
-#  temps, or any of the losses.
-#  Also need to figure out what is going on with the pressure conversion b/c
-#  pressures don't come out just right consistently (although usually within a
-#  pound or two)
 
 
 with open(save_filepath, mode='w', encoding='utf-8') as file:
